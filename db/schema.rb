4988a4cfbbce33e992219a5695e02bde4a7731f6
jaskarnsingh2 <jaskarnsingh2@academic.rrc.ca>
2025-03-19 13:14:02 -0500

    new author model created

--- db/schema.rb
+++ db/schema.rb
@@ -33,3 +33,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_18_224923) do
 
   add_foreign_key "magazines", "books"
 end
+
+create_table "authors", force: :cascade do |t|
+  t.string "name"
+  t.date "birth_date"
+  t.text "biography"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
+create_table "categories", force: :cascade do |t|
+  t.string "name"
+  t.text "description"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
