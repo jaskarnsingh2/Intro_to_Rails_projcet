012cb6187d413b975fc899f9fca9964143d7b265
jaskarnsingh2 <jaskarnsingh2@academic.rrc.ca>
2025-03-19 14:05:23 -0500

    new controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,44 @@
+# Rails.application.routes.draw do
+#   get 'magazines/show_all'
+#   get 'books/show_all'
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+#   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+#   # Can be used by load balancers and uptime monitors to verify that the app is live.
+#   get "up" => "rails/health#show", as: :rails_health_check
+
+#   # Defines the root path route ("/")
+#   # root "posts#index"
+# end
+
 Rails.application.routes.draw do
   root 'home#index'
 
   get 'about', to: 'about#index'
-  
-  # Books routes
+  get 'books', to: 'books#show_all'
+  get 'magazines', to: 'magazines#show_all'
+  resources :authors, only: [:index, :show]
+  resources :categories, only: [:index, :show]
+  resources :publishers, only: [:index, :show]
+
   resources :books, only: [:show] do
     resources :magazines, only: [:show], controller: 'books/magazines'
   end
-  
-  resources :books, only: [:index, :show] do
-    collection do
-      get 'show_all'
-    end
-  end
 
-  # Magazines routes
-  resources :magazines, only: [:show] do
-    collection do
-      get 'show_all'
-    end
-  end
-  
-  # Authors routes
-  resources :authors, only: [:index, :show]
-
-  # Categories routes
-  resources :categories, only: [:index, :show]
-
-  # Publishers routes
-  resources :publishers, only: [:index, :show]
-
-  # Reviews routes
-  resources :reviews, only: [:create, :destroy]
+  resources :magazines, only: [:show]
 
   # Routes for showing the search
   resources :books do
     collection do
-      get 'show_all'
+      get 'show_all' 
     end
   end
 
   resources :magazines do
     collection do
-      get 'show_all'
+      get 'show_all' 
     end
   end
+  
+
 end
