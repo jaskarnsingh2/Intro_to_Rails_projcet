a2f9e1309320fa0087c5430c4f4b8790ddb04e08
jaskarnsingh2 <jaskarnsingh2@academic.rrc.ca>
2025-03-19 23:23:05 -0500

    added the reviews section

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,6 @@ class Book < ApplicationRecord
     # Associations
     has_many :authors, through: :book_authors
     has_many :magazines
-    has_and_belongs_to_many :authors
     belongs_to :publisher
     belongs_to :category
     has_many :reviews, dependent: :destroy
